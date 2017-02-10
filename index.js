b4288a4eaf0f3164a037d82281af4bc451eff516
Julian <jperelli@gmail.com>
2017-02-10 16:38:42 -0300

    Adding plugin installation and listing feature

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+var os = require("os");
+var spawn = require('child_process').spawn;
+var https = require('https');
 
 var PORT = process.env.PORT || 43569;
 app.set('port', PORT);
@@ -9,7 +12,61 @@ app.use(function (req, res, next) {
 })
 
 app.get('/', function (req, res) {
-  res.send('')
+  res.json({
+    domokeeper: process.env.npm_package_version,
+    hostname: os.hostname()
+  })
+})
+
+app.get('/plugins/', function (req, res) {
+  var child = spawn('npm', ['ls', '--json', '--depth', '0']);
+  var buff = '';
+  child.stdout.on('data', function(data) {
+    buff += data;
+  });
+  child.on('close', function(code) {
+    var dependencies = JSON.parse(buff).dependencies;
+    var ds = [];
+    for (var name in dependencies)
+      if (name.indexOf('domokeeper-plugin') == 0)
+        ds.push({name:name, version:dependencies[name].version});
+    res.json(ds);
+  });
+})
+
+app.post('/plugins/install/:id', function (req, res) {
+  var child = spawn('npm', ['install', req.params.id]);
+  var stdout = '';
+  child.stdout.on('data', function(data) {
+    stdout += data;
+  });
+  child.on('close', function(code) {
+    res.json({stdout: stdout, code: code});
+  });
+})
+
+app.get('/plugins/install/', function (req, res) {
+  https.get({
+    host: 'www.npmjs.com',
+    path: '/-/search?text=domokeeper-plugin',
+    headers: {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'},
+  }, function(response) {
+      var body = '';
+      response.on('data', function(d) { body += d; });
+      response.on('end', function()   {
+        body = JSON.parse(body);
+        var r = [];
+        for (var i = 0; i < body.objects.length; i++) {
+          r.push(body.objects[i].package);
+        }
+        res.json(r);
+      });
+  });
+})
+
+app.get('/plugins/:id', function (req, res) {
+  var plugin = require(req.params.id);
+  res.json(plugin);
 })
 
 app.listen(PORT, function () {
